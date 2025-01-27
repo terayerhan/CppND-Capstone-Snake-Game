b6e1aa3e412822fd6545523a5a5b9bd30240f2e9
Terungwa_Ayerhan <terayerhan@outlook.com>
2025-01-28 10:15:41 +1100

    Update Grid constructor initialize SDLPointHash::grid_width

--- src/grid.cpp
+++ src/grid.cpp
@@ -1,13 +1,17 @@
 #include "grid.h"
+#include "sdl_point_operators.h"
 
-Grid::Grid(int width, int height) : _width(width), _height(height) {}
+Grid::Grid(int width, int height) : _width(width), _height(height) {
+    // Initialize grid width for hash function
+    SDLPointHash::grid_width = width;
+}
 
 std::pair<float, float> Grid::WrapPosition(float x, float y) const {
     if (x < 0) x += _width;
     if (x >= _width) x -= _width;
     if (y < 0) y += _height;
     if (y >= _height) y -= _height;
-    
+
     return {x, y};
 }
 
