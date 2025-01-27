75a99750d9aaadcdfa88db864ff0406154b0d9bf
Terungwa_Ayerhan <terayerhan@outlook.com>
2025-01-28 09:52:19 +1100

    Add sdl_point_operators.h SDL_Point comparisions
    This adds overloaded comparison operators need for working with stl
    algorithms like sort, find etc and there is a grid optimized SDL_Point
    hash funtor for use with unordered_sets in conjunction with the ==
    overloaded operator.

---
+++ src/sdl_point_operators.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+// Equality operator (needed for unordered_set)
+inline bool operator==(const SDL_Point& lhs, const SDL_Point& rhs) {
+   return (lhs.x == rhs.x && lhs.y == rhs.y);
+}
+
+// Inequality operator 
+inline bool operator!=(const SDL_Point& lhs, const SDL_Point& rhs) {
+   return !(lhs == rhs);
+}
+
+// Less than operator (needed for ordered containers and std::sort)
+inline bool operator<(const SDL_Point& lhs, const SDL_Point& rhs) {
+    return (lhs.x < rhs.x) || (lhs.x == rhs.x && lhs.y < rhs.y);
+}
+
+/* // Greater than operator
+inline bool operator>(const SDL_Point& lhs, const SDL_Point& rhs) {
+   return rhs < lhs;
+}
+
+// Less than or equal operator
+inline bool operator<=(const SDL_Point& lhs, const SDL_Point& rhs) {
+   return !(rhs < lhs);
+}
+
+// Greater than or equal operator
+inline bool operator>=(const SDL_Point& lhs, const SDL_Point& rhs) {
+   return !(lhs < rhs);
+} */ // I don't think I need these once for now.
+
+// Hash function optimized for grid-based games (needed for unordered_set)
+struct SDLPointHash {
+    static int grid_width;
+    
+    std::size_t operator()(const SDL_Point& p) const {
+        return p.x * grid_width + p.y;
+    }
+};
