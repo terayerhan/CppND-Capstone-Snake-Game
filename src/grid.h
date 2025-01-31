6e56c2222c28211d51eb15ca8d19479b87b1d929
Terungwa_Ayerhan <terayerhan@outlook.com>
2025-01-31 12:51:58 +1100

    Inline the Grid methods

--- src/grid.h
+++ src/grid.h
@@ -4,16 +4,16 @@
 
 class Grid {
  public:
-    Grid(size_t width, size_t height);
+   Grid(size_t width, size_t height);
 
-    size_t GetWidth() const { return _width; }
-    size_t GetHeight() const { return _height; }
+   inline size_t GetWidth() const { return _width; }
+   inline size_t GetHeight() const { return _height; }
 
-    // Wraps floating point coordinates
-    std::pair<float, float> WrapPosition(float x, float y) const;
+   // Wraps floating point coordinates
+   inline std::pair<float, float> WrapPosition(float x, float y) const;
 
-    // Wrap integer position
-    std::pair<int, int> WrapPosition(int x, int y) const;
+   // Wrap integer position
+   inline std::pair<int, int> WrapPosition(int x, int y) const;
 
  private:    
     /* data */
