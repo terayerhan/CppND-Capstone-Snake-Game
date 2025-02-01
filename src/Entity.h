52f220c6d1413b4a65617b193d2a44040ef061ec
Terungwa_Ayerhan <terayerhan@outlook.com>
2025-02-01 13:26:46 +1100

    Add GetCellX() and GetCellY() to Entity class
    This getters will return the main or head or most relevant position of an
    Entity. In its default implementation, it calls the GetPosition to retrive
    the cell components but subclasses can override it to provide a better
    implementation.

--- src/Entity.h
+++ src/Entity.h
@@ -7,6 +7,8 @@ public:
     virtual ~Entity() = default;
     virtual EntityType GetType() const = 0;
     virtual SDL_Point GetPosition() const = 0;
+    virtual int GetCellX() const {return GetPosition().x;}
+    virtual int GetCellY() const {return GetPosition().y;}
     virtual bool IsActive() const = 0;
     
 };
