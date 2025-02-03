03002808f3f3c7a6dd399bf9cbd614d4943af16a
Terungwa_Ayerhan <terayerhan@outlook.com>
2025-02-03 12:47:14 +1100

    Complete implementation of AISnake::AddNode()

--- src/snakes.cpp
+++ src/snakes.cpp
@@ -8,17 +8,20 @@ std::shared_ptr<Node> AISnake::AddNode( std::shared_ptr<Node> current, Direction
     float nextHeadX = current->headX_;         // Use this to update nextHeadCellX
     float nextHeadY = current->headY_;
 
-    SDL_Point currentCell = current->cell_;
-    SDL_Point nextHeadCell = currentCell;
+    SDL_Point currentCell = current->cell_;  // Cach the current node's cell.
+    SDL_Point nextHeadCell = currentCell;    // initialize the nextHeadCell.
 
-    size_t nextTimeStep = current->gCost_;
+    /* Initialize the next node's time step from the current node's gCost. gCost the number of (time)steps 
+       it will take the head of a snake to reach a position (float) not just the Cell position. 
+    */
+    size_t nextTimeStep = current->gCost_;   
 
-    float speed = GetSpeed();
+    float speed = GetSpeed();  // Use the Snake's speed to determine the distance traveled by snake per step.
 
     // While still in the current nodes's cell, keep stepping forward till you reach the next cell.
     while(nextHeadCell == currentCell) {
-        // Move one step.
-        switch (_direction) {
+        // Move one step in the nextDirection.
+        switch (nextDirection) {
             case Direction::kUp:
                 nextHeadY -= speed;
                 break;
@@ -43,6 +46,9 @@ std::shared_ptr<Node> AISnake::AddNode( std::shared_ptr<Node> current, Direction
 
         nextHeadCell.x = static_cast<int>(nextHeadX);
         nextHeadCell.y = static_cast<int>(nextHeadY);
+
+        // Increment time steps by 1.
+        nextTimeStep++;
 
         /* Since  head is moving till it reaches the next cell, check if this simulatd snake head
            has reached the next cell so that its bodycells can be updated before checking for 
@@ -73,11 +79,13 @@ std::shared_ptr<Node> AISnake::AddNode( std::shared_ptr<Node> current, Direction
             */
             if (_predictedPlayerBlockedCells.count(nextTimeStep)) {
                 /* nextTimeStep exist in the blockedCells unordered_maps, check for collision
-                   playerSnake and self 
+                   playerSnake and self. 
                 */
-                if (_predictedPlayerBlockedCells.at(nextTimeStep).count(nextHeadCell) ||
-                    std::find(currentBodyCells.rbegin() + 1, currentBodyCells.rend(), nextHeadCell)
-                      != currentBodyCells.rend()
+                if (
+                    _predictedPlayerBlockedCells[nextTimeStep].count(nextHeadCell) ||
+                    std::find(
+                        currentBodyCells.begin() + 1, currentBodyCells.end(), nextHeadCell)
+                        != currentBodyCells.end()
                     ) {
                     // Taking this direction from the current node will result in a collision with either 
                     // player sanke or self. Return nullptr to indicate this.
@@ -86,9 +94,73 @@ std::shared_ptr<Node> AISnake::AddNode( std::shared_ptr<Node> current, Direction
             }
             else {
                 /* nextTimeStep does not exist Generate more time steps enough to reach the goal(food) */
+                GenerateBlockedCells(
+                    nextTimeStep,
+                    CalculateHeuristic(
+                        nextHeadX, nextHeadY, speed, _food.GetCellX(), _food.GetCellY(),
+                        _grid.GetWidth(), _grid.GetHeight()
+                    )
+                );
+
+                /* nextTimeStep Should Now exist in the blockedCells unordered_maps, check for collision
+                   playerSnake and self. 
+                */
+                if (_predictedPlayerBlockedCells[nextTimeStep].count(nextHeadCell) ||
+                    std::find(currentBodyCells.rbegin() + 1, currentBodyCells.rend(), nextHeadCell)
+                      != currentBodyCells.rend()
+                    ) {
+                    return nullptr;
+                }
             }
 
+        }  // End checking if snake will reach next cell.
+
+        // Since head will still be in the current cell, only check collision with Obstacles.
+        if (_predictedObstacleBlockedCells.count(nextTimeStep)) {
+                /* nextTimeStep exist in the blockedCells unordered_maps, check if any ObstacleSnakes will
+                   collide with this snake or if this snake will collide with any obstacleSnake.
+                */
+                if (_predictedObstacleBlockedCells[nextTimeStep].count(nextHeadCell) ) {
+                    /* Taking this direction from the current node will result in a collision with an 
+                       ObstacleSnake or an ObstacleSnake will collide with this snake. Return nullptr 
+                       to indicate this. 
+                    */
+                    return nullptr;
+                }
         }
-    }
+        else {
+            /* nextTimeStep does not exist Generate more time steps enough to reach the goal(food) */
+            GenerateBlockedCells(
+                nextTimeStep,
+                CalculateHeuristic(
+                    nextHeadX, nextHeadY, speed, _food.GetCellX(), _food.GetCellY(),
+                    _grid.GetWidth(), _grid.GetHeight()
+                )
+            );
+
+            /* nextTimeStep Should Now exist in the blockedCells unordered_maps, check if any 
+                ObstacleSnakes will collide with this snake or if this snake will collide with any 
+                obstacleSnake.
+            */
+            if (_predictedObstacleBlockedCells[nextTimeStep].count(nextHeadCell)) { return nullptr; }
+        }
+    } // End of while loop moving snake and checking collisions.
+
+    /* Snake will arrive in next cell with no collision againt or by ObstacleSnake and no collision with
+       playerSnake or self when it take nextDirection from current position in current cell. return 
+       shared_ptr to resulting Node.
+    */
+    return std::make_shared<Node>(
+        nextHeadCell,
+        nextTimeStep,
+        nextTimeStep + CalculateHeuristic(
+            nextHeadX, nextHeadY, speed, _food.GetCellX(), _food.GetCellY(), 
+            _grid.GetWidth(), _grid.GetHeight()
+        ),
+        nextHeadX,
+        nextHeadY,
+        nextDirection,
+        current
+    );
     
 }
