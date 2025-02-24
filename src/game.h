e17d41c96571fe10b5c709865a3f6bfeaa336a45
Terungwa_Ayerhan <terayerhan@outlook.com>
2025-02-24 15:16:29 +1100

    Modify the Game class to declare snakes, and GetEmptyCell()
    Also added a vector of snake pointers to hold a link to all the snakes
    so that GetEmptyCell() can luse to find an empty cell to place food, or
    to initialize the playerSnake and AISnake.

--- src/game.h
+++ src/game.h
@@ -6,18 +6,24 @@
 #include "controller.h"
 #include "renderer.h"
 #include "snake.h"
+#include "snakes.h"
 
 class Game {
  public:
   Game(std::size_t grid_width, std::size_t grid_height);
+
   void Run(Controller const &controller, Renderer &renderer,
            std::size_t target_frame_duration);
+           
   int GetScore() const;
   int GetSize() const;
 
  private:
-  Snake snake;
-  SDL_Point food;
+  std::vector<ObstacleSnake> _obstacles;
+  PlayerSnake _playerSnake;
+  Food _food;
+
+  std::vector<Snake*> _allSnakes_ptrs;
 
   std::random_device dev;
   std::mt19937 engine;
@@ -25,6 +31,8 @@ class Game {
   std::uniform_int_distribution<int> random_h;
 
   int score{0};
+
+  SDL_Point GetEmptyCell();
 
   void PlaceFood();
   void Update();
