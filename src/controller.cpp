4adc1d2c034db586adf1a0fe65354076ffa3945f
Terungwa_Ayerhan <terayerhan@outlook.com>
2025-03-11 12:41:28 +1100

    Update Controller Class to use snakes.h and global Directions enum class

--- src/controller.cpp
+++ src/controller.cpp
@@ -3,13 +3,12 @@
 #include "SDL.h"
 #include "snake.h"
 
-void Controller::ChangeDirection(Snake &snake, Snake::Direction input,
-                                 Snake::Direction opposite) const {
-  if (snake.direction != opposite || snake.size == 1) snake.direction = input;
+void Controller::ChangeDirection(PlayerSnake &playerSnake, Direction input, Direction opposite) const {
+  if (playerSnake._direction != opposite || playerSnake.size == 1) playerSnake._direction = input;
   return;
 }
 
-void Controller::HandleInput(bool &running, Snake &snake) const {
+void Controller::HandleInput(bool &running, PlayerSnake &playerSnake) const {
   SDL_Event e;
   while (SDL_PollEvent(&e)) {
     if (e.type == SDL_QUIT) {
@@ -17,23 +16,19 @@ void Controller::HandleInput(bool &running, Snake &snake) const {
     } else if (e.type == SDL_KEYDOWN) {
       switch (e.key.keysym.sym) {
         case SDLK_UP:
-          ChangeDirection(snake, Snake::Direction::kUp,
-                          Snake::Direction::kDown);
+          ChangeDirection(playerSnake, Direction::kUp, Direction::kDown);
           break;
 
         case SDLK_DOWN:
-          ChangeDirection(snake, Snake::Direction::kDown,
-                          Snake::Direction::kUp);
+          ChangeDirection(playerSnake, Direction::kDown, Direction::kUp);
           break;
 
         case SDLK_LEFT:
-          ChangeDirection(snake, Snake::Direction::kLeft,
-                          Snake::Direction::kRight);
+          ChangeDirection(playerSnake, Direction::kLeft, Direction::kRight);
           break;
 
         case SDLK_RIGHT:
-          ChangeDirection(snake, Snake::Direction::kRight,
-                          Snake::Direction::kLeft);
+          ChangeDirection(playerSnake, Direction::kRight, Direction::kLeft);
           break;
       }
     }
--- src/controller.h
+++ src/controller.h
@@ -1,15 +1,14 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#include "snake.h"
+#include "snakes.h"
 
 class Controller {
  public:
-  void HandleInput(bool &running, Snake &snake) const;
+  void HandleInput(bool &running, PlayerSnake &playerSnake) const;
 
  private:
-  void ChangeDirection(Snake &snake, Snake::Direction input,
-                       Snake::Direction opposite) const;
+  void ChangeDirection(PlayerSnake &playerSnake, Direction input, Direction opposite) const;
 };
 
 #endif
