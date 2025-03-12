2463afe7dca1f03aa9addf460fbe526cbb0cf1e2
Terungwa_Ayerhan <terayerhan@outlook.com>
2025-03-12 11:08:59 +1100

    Fix: error: ‘_width’ was not declared in this scope
    The error seamed to be occuring because I declared WrapPosition methods
    as inline in the header file, but then I also defining them in the
    .cpp file. This created some conflicts.

--- src/grid.h
+++ src/grid.h
@@ -1,25 +1,36 @@
 #pragma once
 
 #include <utility>
+#include "sdl_point_operators.h"  
 
 class Grid {
  public:
-   Grid(size_t width, size_t height);
+  Grid(std::size_t width, std::size_t height) : _width(width), _height(height) {
+    // Initialize grid width for hash function
+    SDLPointHash::grid_width = width;
+  }
 
-   inline size_t GetWidth() const { return _width; }
-   inline size_t GetHeight() const { return _height; }
+  inline std::size_t GetWidth() const { return _width; }
+  inline std::size_t GetHeight() const { return _height; }
 
-   // Wraps floating point coordinates
-   inline std::pair<float, float> WrapPosition(float x, float y) const;
+  inline std::pair<float, float> WrapPosition(float x, float y) const {
+    // Wraps coordinates around the grid boundaries (toroidal wrapping).
+    if (x < 0) x += _width;
+    if (x >= _width) x -= _width;
+    if (y < 0) y += _height;
+    if (y >= _height) y -= _height;
+    return {x, y};
+  }
 
-   // Wrap integer position
-   inline std::pair<int, int> WrapPosition(int x, int y) const;
+  inline std::pair<int, int> WrapPosition(int x, int y) const {
+    if (x < 0) x += _width;
+    if (x >= _width) x -= _width;
+    if (y < 0) y += _height;
+    if (y >= _height) y -= _height;
+    return {x, y};
+  }
 
  private:    
-    /* data */
-    size_t _width;
-    size_t _height;
-    
+  std::size_t _width;
+  std::size_t _height;
 };
-
-
