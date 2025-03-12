f3783ca9950cffd11fafe97d3c855571704f3bf2
Terungwa_Ayerhan <terayerhan@outlook.com>
2025-03-13 10:46:33 +1100

    BugFix: player score not been updated on the titlebar

--- src/game.cpp
+++ src/game.cpp
@@ -78,7 +78,7 @@ void Game::Run(Controller const &controller, Renderer &renderer,
 
     // After every second, update the window title.
     if (frame_end - title_timestamp >= 1000) {
-      renderer.UpdateWindowTitle(score, frame_count);
+      renderer.UpdateWindowTitle(GetScore(), frame_count);
       frame_count = 0;
       title_timestamp = frame_end;
     }
