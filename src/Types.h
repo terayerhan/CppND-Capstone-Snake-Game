27cba2a7214d6cfa20922768ccd5890f0898a4d9
Terungwa_Ayerhan <terayerhan@outlook.com>
2025-03-20 10:22:35 +1100

    Add OppositeDirection() in Types.h to return the opposite of a OppositeDirection
    This is primarily for use in the neighbor node exporation method of the
    AISnake::FindPath() method to prevent the snake from adding nodes that
    will cause it to reverse onto its body when it size is greater than one
    cell.

--- src/Types.h
+++ src/Types.h
@@ -11,6 +11,23 @@ enum class EntityType {
 
 enum class Direction { kUp, kDown, kLeft, kRight }; 
 
+/**
+ * @brief Returns the opposite direction of the given direction.
+ *
+ * @param d The input direction.
+ * @return Direction The direction opposite to d.
+ */
+Direction OppositeDirection(Direction d) {
+    switch(d) {
+        case Direction::kUp:    return Direction::kDown;
+        case Direction::kDown:  return Direction::kUp;
+        case Direction::kLeft:  return Direction::kRight;
+        case Direction::kRight: return Direction::kLeft;
+        default:                return d; // Alternatively, throw an exception or assert.
+    }
+}
+
+
 // Hash function for pairs
 struct PairHash {
     template <typename T1, typename T2>
