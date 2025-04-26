29de57e416a6fc43f79fae779d307e265ef92090
Terungwa_Ayerhan <terayerhan@outlook.com>
2025-04-26 14:10:26 +1000

    Remove PairHash struct that is no longer been used

--- src/Types.h
+++ src/Types.h
@@ -27,16 +27,3 @@ inline Direction OppositeDirection(Direction d) {
     }
 }
 
-
-// Hash function for pairs
-struct PairHash {
-    template <typename T1, typename T2>
-    std::size_t operator()(const std::pair<T1, T2>& p) const {
-        // Hash each element in the pair
-        auto hash1 = std::hash<T1>{}(p.first);
-        auto hash2 = std::hash<T2>{}(p.second);
-
-        // Combine the two hashes
-        return hash1 ^ (hash2 << 1);  // XOR and bit-shifting to combine
-    }
-};
